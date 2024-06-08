a396e16607ad42c0dbef68ab742df6204145c3ed
AmeyKuradeAK <ameykurade60@gmail.com>
2024-06-08 18:07:18 +0530

    Init

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="mx-2 w-10">AK</h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/ameykuradeak/">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://github.com/AmeyKuradeAK">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/__s_a_g_e/">
+          <FaInstagram />
+        </a>
+        <a href="https://x.com/KuradeAmey">
+          <FaSquareXTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a full-stack developer with a passion for bringing ideas to life through code and art. I leverage the MERN stack for development and my expertise in Python to craft exceptional user experiences. But my talents extend beyond the screen - I'm also a professional 3D artist with skills in CGI and VFX.  Let's collaborate and create something extraordinary.`;
+
+export const ABOUT_TEXT = `I'm a full-stack developer who thrives at the intersection of code and creativity. I leverage the MERN stack and my Python mastery to build web applications that are not only functional but also visually engaging. But my skillset doesn't stop there. As a professional 3D artist with expertise in CGI and VFX, I can bring your ideas to life in stunning detail. I'm a lifelong learner, constantly seeking new ways to push the boundaries and craft immersive, interactive experiences. Let's collaborate and transform your vision into a reality that's both beautiful and functional.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Studying in FY CSE",
+    company: "DYPSEM",
+    description: `Studying FY CSE in DYPSEM`,
+    technologies: ["Blender", "React.js", "Python"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Drop out CSE",
+    company: "SGU",
+    description: `Dropped out of old college where I was learning CSE`,
+    technologies: ["HTML", "CSS", "mySQL"],
+  },
+  {
+    year: "2020 - 2022",
+    role: "Preparing for JEE",
+    company: "SGIMA",
+    description: `I was in Academy for the preperation of JEE`,
+    technologies: ["PCM", "C++"],
+  },
+  {
+    year: "2017 - 2020",
+    role: "School Student",
+    company: "Sanjeevan Public School",
+    description: `HighSchool`,
+    technologies: ["HTML", "CSS", "C"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Framer Motion", "JS"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["Wordpress", "cPanel"],
+  },
+];
+
+export const CONTACT = {
+  address: "Panhala, Kolhapur, Maharashtra, 416201",
+  phoneNo: "+91 706640 5064 ",
+  email: "ameykurade60@gmail.com",
+};
